687e9e06d406a7136bdf8d370e3dbae52327481b
jodelajo <jodelajo@gmail.com>
2022-08-06 15:27:01 +0200

    create database

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,31 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing one',
-                'description' => 'Learn the Laravel PHP framework from scratch by building a job listings application with Laravel 9 and MySQL.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing two',
-                'description' => 'Thank you Brad for this. I was literally just going through your PHP course when you popped this one up. I hope you and your family are well and welcome back.'
-            ]
-        ];
-    }
-
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
-};
+    use HasFactory;
+}
